ca53f0d1300d02ee380d61baaff5d523f9efc5c8
Konsti2709 <konstantin.fedine@icloud.com>
2026-07-12 10:32:54 +0200

    Footer hinzugefügt und Komponenten-Laden verbessert
    - Wiederverwendbaren Footer erstellt
    - Footer-Design mit responsive Layout hinzugefügt
    - load-components.js zum Laden von Header und Footer angepasst
    - Initialisierung von Header und FAQ verbessert

--- js/load-components.js
+++ js/load-components.js
@@ -3,6 +3,23 @@ fetch("components/header.html")
     .then(data => {
         document.body.insertAdjacentHTML("afterbegin", data);
 
-        if (typeof initHeader === "function") initHeader();
-        if (typeof initFAQ === "function") initFAQ();
+        if (typeof initHeader === "function") {
+            initHeader();
+        }
     });
+
+
+fetch("components/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.body.insertAdjacentHTML("beforeend", data);
+    });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    if (typeof initFAQ === "function" && document.querySelector(".faq-item")) {
+        initFAQ();
+    }
+
+});
